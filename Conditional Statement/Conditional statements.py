b954249b36c04b49e9273072f488ab9ca1972db8
Aarush2710 <aarush.indulkar2710@gmail.com>
2026-03-20 20:50:06 +0530

    Add files via upload

---
+++ Conditional Statement/Conditional statements.py
@@ -0,0 +1,9 @@
+
+# write a program to check whether the given number is positive or not?
+num=int(input("Enter the number"))
+if num > 0:
+    print("The given number is positive")
+elif num < 0:
+    print("the given number is negative")
+else:
+    print("the given number is 0")
---
+++ Conditional Statement/profit loss.py
@@ -0,0 +1,7 @@
+# profit or loss
+cost_price=int(input("Enter the cost price  "))
+selling_price=int(input("Enter the selling price  "))
+if selling_price > cost_price:
+    print("profit")
+else:
+    print("loss")
